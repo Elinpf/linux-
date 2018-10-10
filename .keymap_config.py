fe142dcaff3cfd676eddc7482f839f62c94b2896
Elinpf <365433079@qq.com>
2018-10-10 11:42:20 +0800

    加入了hjkl

--- .keymap_config.py
+++ .keymap_config.py
@@ -4,4 +4,12 @@ from xkeysnail.transform import *
 define_multipurpose_modmap(
     {Key.CAPSLOCK: [Key.ESC, Key.LEFT_CTRL]}
 )
+
+define_keymap(None, {
+    K("M-h"): K("left"),
+    K("M-l"): K("right"),
+    K("M-k"): K("up"),
+    K("M-j"): K("down")
+})
+
     
